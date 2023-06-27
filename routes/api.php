45cdd9920200ff6b7c7e2f0b975536abb1df100c
niiaddoquaynor <laryea.enl99@gmail.com>
2023-06-27 22:12:35 +0000

    authentication api completed

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// for basic crud apis, use this single line
+// Route::resource('products', ProductController::class);
+
+// Public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
